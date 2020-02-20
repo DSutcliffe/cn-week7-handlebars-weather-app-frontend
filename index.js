a7b6d08d4129e0c06579dfb2d67d43186a22d92b
Code Nation <codenation@Codes-MacBook-Pro.local>
2020-02-20 16:49:15 +0000

    New Entry Fields

--- index.js
+++ index.js
@@ -1,10 +1,11 @@
 const hbs = require(`express-handlebars`);
 const path = require(`path`);
+const bodyParser = require('body-parser');
 const express = require(`express`);
 
 const app = express();
 
-// Import the getWeather function
+// Import the getWeather function from getWeather.js
 const getWeather = require('./lib/getWeather');
 
 // use tells express to use this folder
@@ -12,6 +13,8 @@ const getWeather = require('./lib/getWeather');
 // path is used to join these 2 paths
 // this html stuff is in the 'public' folder
 app.use(express.static(path.join(__dirname, `public`)));
+app.use(bodyParser.urlencoded({extended: false}));
+app.use(bodyParser.json());
 
 // set view engine to handlebars
 // extname sets extension to .hbs
@@ -28,21 +31,28 @@ app.set('view engine', '.hbs');
 //     res.render('index');
 // });
 
+app.get('/', (req, res) => {
+    res.render('index');
+})
 // Make function asynchronous
-app.get('/', async(req, res) => {
+app.post('/', async(req, res) => {
+    let location = req.body.location;
+    let countryCode = req.body.countryCode;
+    console.log(location);
+
     // Wait for getWeather function to run and store in the 'data'
-    let data = await getWeather();
+    let data = await getWeather(location, countryCode);
 
     // data is already made up of data.body - return data.body in getWeather
     console.log(data)
-    console.log(data.name)
+    // console.log(data.name)
 
     let temp = (data.main.temp - 273).toFixed(2);
     let city = data.name;
 
     // render index.hbs page
     // show temp (temp must be stated in index.hbs <p>{{...}}</p>)
-    res.render('index', {temp, city});
+    res.render('index', {data: {temp, city}});
 });
 
 app.listen(3000, () => {
